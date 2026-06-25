a84a822d749475eeb546c066217776f000463f8d
harshitha cs <144522379+csharshitha088@users.noreply.github.com>
2026-06-25 10:59:31 +0530

    Update footer credits from Mayuri K to Harshitha CS

--- footer/footer.php
+++ footer/footer.php
@@ -9,8 +9,8 @@
       </ul>
    </div>
    <div class="copyright">
-      <div class="footer text-center">Design & Develop By Mayuri K | Visit : www.mayurik.com</div>
-      &copy; MayuriK Artwork Online Studio.All rights reserved.
+      <div class="footer text-center">Design & Develop By Harshitha CS</div>
+      &copy; Harshitha CS Artwork Online Studio.All rights reserved.
    </div>
 </footer>
 <!-- Scripts -->
